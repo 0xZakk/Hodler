c27640ca881a0e1157dcac921669fb87e2dcd3c6
Zakk <zakk@hawthorne.io>
2022-10-07 13:47:26 -0500

    add token interface; add doc strings;

--- src/Hodler.sol
+++ src/Hodler.sol
@@ -1,31 +1,37 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
+interface Token {
+    function balanceOf(address) external returns (uint);
+}
+
 contract Hodler {
     event TokenHoldUpdated(address indexed newTokenAddress);
+    error NotHodling();
 
-    address public tokenToHold;
+    Token public tokenToHold;
 
     constructor(address _token) {
-        tokenToHold = _token;
+        tokenToHold = Token( _token );
 
         emit TokenHoldUpdated(_token);
     }
 
-    // is holding the token
-    function _isHodling() internal view virtual {
-        require(
-            address(tokenToHold).balanceOf(msg.sender) >= 1,
-            ""
-        );
+    /// @notice Function to check and see if someone holds a token
+    /// @dev Intended to be called inside your own functions to check that
+    ///      someone holds a funtion
+    function _isHodling(address account) internal virtual returns (bool) {
+        return tokenToHold.balanceOf(account) >= 1;
     }
 
-    modifier onlyHolder() virtual {
+    /// @notice Modifier to restrict function invocation to someone who holds
+    ///         a token.
+    modifier onlyHolder() {
         // check if holding
-        require();
+        if(tokenToHold.balanceOf(msg.sender) == 0) {
+            revert NotHodling();
+        }
 
         _;
     }
-
-    // change token
 }
